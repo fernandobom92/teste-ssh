f8070442a04dac37923647496c84d5ac5c7c2f29
Fernando Souza <fernandobom92@gmail.com>
2022-06-08 12:27:17 -0300

    Teste

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('Ola mundo!');
+const { 
+    Engine, 
+    Render, 
+    Runner, 
+    World, 
+    Bodies, 
+ } = Matter;
+
+const width = 600;
+const height = 600;
+
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        width: width,
+        height: height,
+        wireframes: true
+    }
+});
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+//walls
+const walls = [
+    Bodies.rectangle(width / 2, 0, width, 40, { isStatic:true }),
+    Bodies.rectangle(width / 2, height, width, 40, { isStatic:true }),
+    Bodies.rectangle(0, height / 2, 40, height, { isStatic:true }),
+    Bodies.rectangle(width, height / 2, 40, height, { isStatic:true })
+];
+World.add(world,walls);
